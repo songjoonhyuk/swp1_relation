226da0dd060b0a9ceca0b09ada7b1618cfbd2c4c
songjoonhyuk <songjoonhyuk@gmail.com>
2015-10-28 11:22:08 +0000

    final

--- db/migrate/20151022061806_devise_create_users.rb
+++ db/migrate/20151022061806_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.integer :user_id
+      t.string :nickname, null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
